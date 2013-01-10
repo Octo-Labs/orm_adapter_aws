4f11df60984a63c3e298db7d801b95290b1186b3
Jeremy Green <jeremy@octolabs.com>
2013-01-10 13:46:43 -0600

    Initial implementation is complete.  The orm_adapter shared example excercises some functions that aren't suppored by SimpleDB.  Those tests fail.

--- lib/orm_adapter_aws/adapters/simple_db.rb
+++ lib/orm_adapter_aws/adapters/simple_db.rb
@@ -21,25 +21,37 @@ module OrmAdapterAws
     # @see OrmAdapter::Base#find_first
     def find_first(options = {})
       conditions, order = extract_conditions!(options)
-      scope = klass.where(conditions_to_fields(conditions))
-      scope = scope.order(order) if !order.nil? && order.size > 0
-      scope.first
+      id = nil
+      if conditions.keys.include? :id
+        id = conditions.delete(:id)
+        obj = get(id)
+        compare_obj_to_conditions(obj,conditions)
+      else
+        scope = klass.where(conditions_to_fields(conditions))
+        scope = scope.order(*order_munge(order)) if !order.nil? && order.size > 0
+        obj = scope.first
+      end
     end
 
     # @see OrmAdapter::Base#find_all
     def find_all(options = {})
       conditions, order, limit, offset = extract_conditions!(options)
       scope = klass.where(conditions_to_fields(conditions))
-      scope = scope.order(order) if !order.nil? && order.size > 0
+      scope = scope.order(*order_munge(order)) if !order.nil? && order.size > 0
       scope = scope.limit(limit) if !limit.nil?
       #.offset(offset).all
       scope.all.to_a
     end
 
     # @see OrmAdapter::Base#create!
-    #def create!(attributes = {})
-      #klass.create!(attributes)
-    #end
+    def create!(attributes = {})
+      unless attributes.is_a?(Hash) || valid_object?(attributes)
+        raise "#{attributes.class} is not a valid #{klass}."
+      end
+      obj = klass.new(attributes)
+      obj.save
+      obj
+    end
 
     # @see OrmAdapter::Base#destroy
     def destroy(object)
@@ -47,6 +59,14 @@ module OrmAdapterAws
     end
 
     protected
+    def compare_obj_to_conditions(obj,conditions)
+      return obj if obj.nil?
+      atts = obj.attributes
+      conditions.each_pair do |k,v|
+        return nil unless atts[k] == v
+      end
+      obj
+    end
 
     # Introspects the klass to convert and objects in conditions into foreign key and type fields
     def conditions_to_fields(conditions)
@@ -55,8 +75,19 @@ module OrmAdapterAws
       end
     end
 
-    def order_clause(order)
-      order.map {|pair| "#{pair[0]} #{pair[1]}"}.join(",")
+    def order_munge(order)
+      mung = nil
+      if order.is_a?(Array) && order[0].is_a?(Array)
+        mung = order[0][0], order[0][1]
+      elsif order.is_a? Array
+        mung = order[0],order[1]
+      else
+        mung = order
+      end
+      mung
+      #clause = order.map {|pair| "#{pair[0]} #{pair[1]}"}.join(",")
+      #puts "clause = #{clause} -------------"
+      #clause
     end
 
   end
@@ -67,6 +98,18 @@ module AWS
   module Record
     class Model
       extend ::OrmAdapter::ToAdapter
+
+      #def create!(attributes = nil, options = {}, &block)
+        #if attributes.is_a?(Array)
+          #attributes.collect { |attr| create!(attr, options, &block) }
+        #else
+          #object = new(attributes, options)
+          #yield(object) if block_given?
+          #object.save!
+          #object
+        #end
+      #end
+
     end
   end
 end
--- spec/orm_adapter_aws/adapters/simple_db_spec.rb
+++ spec/orm_adapter_aws/adapters/simple_db_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 require 'orm_adapter/../../spec/orm_adapter/example_app_shared'
+# !!!!!!!!!!!!!!!!!!!!!
+# Not all features in the orm_adapter/example_app_shared spec are supported by SimpleDB.
+# Tests that are known to fail are
+# example_app_shared.rb:100 # no associations
+# example_app_shared.rb:115 # can only sort by one column
+# example_app_shared.rb:151 # no associations
+# example_app_shared.rb:160 # can only sort by one column
+# example_app_shared.rb:188 # no offset for collections
+# example_app_shared.rb:209 # no associations
+# example_app_shared.rb:215 # no associations
+
 
 if !defined?(AWS::Record::Model) #|| !(Mongo::Connection.new.db('orm_adapter_spec') rescue nil)
   puts "** require 'aws-sdk' to run the specs in #{__FILE__}"
@@ -8,22 +19,39 @@ else
   #Mongoid.configure do |config|
     #config.master = Mongo::Connection.new.db('orm_adapter_spec')
   #end
+    class TestBase < AWS::Record::Model
+      ## We have to override == so that we can do comparisons correctly
+      def ==(other)
+        self.attributes == other.attributes
+      end
+
+      def save
+        super
+        sleep(2) # allow a couple of seconds for SimpleDB to propogate
+      end
+
+      def destroy
+        super
+        sleep(2) # allow a couple of secons for SimpleDB to propogate
+      end
+    end
   
-  
-    class User < AWS::Record::Model
+    class User < TestBase
       string_attr :name
       integer_attr :rating
       #has_many_related :notes, :foreign_key => :owner_id, :class_name => 'MongoidOrmSpec::Note'
-      def self.create!(args)
-        u = User.new(args)
-        u.save
-        u
-      end
     end
 
-    class Note < AWS::Record::Model
+    class Note < TestBase
       string_attr :body #, :default => "made by orm"
+      string_attr :owner_id
       #belongs_to_related :owner, :class_name => 'MongoidOrmSpec::User'
+
+      def owner=(owner)
+        owner_id = owner.id
+        save
+      end
+      
     end
   
   # here be the specs!
@@ -33,11 +61,26 @@ else
       Note.create_domain
       User.each{|u| u.destroy }
       Note.each{|n| n.destroy }
+      sleep(2)
     end
   
     it_should_behave_like "example app with orm_adapter" do
       let(:user_class) { User }
       let(:note_class) { Note }
+      def create_model(klass, attrs = {})
+        if klass == User
+          attrs = {:name => "Test User"}.merge(attrs)
+        elsif klass == Note
+          attrs = {:body => "made by orm"}.merge(attrs)
+        end
+        model = klass.new(attrs)
+        model.save
+        model
+      end
+
+      def reload_model(model)
+        model.class.find(model.id)
+      end
     end
   end
   
