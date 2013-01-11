dcf29d5b419fd1d657de81db98ff51a3f903a764
Jeremy Green <jeremy@octolabs.com>
2013-01-11 10:49:53 -0600

    Removed debugging output.

--- lib/orm_adapter_aws.rb
+++ lib/orm_adapter_aws.rb
@@ -1,12 +1,8 @@
 require "orm_adapter_aws/version"
 require "orm_adapter"
 module OrmAdapterAws
-  # Your code goes here...
 end
 
-puts "!!!!!!!"
 if defined?(AWS::Record::Model)
   require 'orm_adapter_aws/adapters/simple_db'
-else
-  puts "can't load our stuff!!!!!!"
 end
